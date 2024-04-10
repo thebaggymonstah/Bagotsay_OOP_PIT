b15ae7fd29510529807d283d8233bc3913fb8c71
thebaggymonstah <thebaggymonstah@gmail.com>
2024-04-10 21:03:37 +0800

    First Commit
    Upload

---
+++ src/Product.java
@@ -0,0 +1,89 @@
+
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Product {
+    private String product_ID;
+    private String product_Name;
+    private float product_Unit_Price;
+    private int product_Quantity;
+    private int sold_Quantity;
+    private double total_Sales;
+    public float discount;
+
+    public Product(){
+
+    }
+    public Product(String product_ID, String product_Name, float product_Unit_Price, int product_Quantity, int sold_Quantity, double total_Sales, boolean isDiscounted, float discount){
+        this.setProduct_ID(product_ID);
+        this.setProduct_Name(product_Name);
+        this.setProduct_Unit_Price(product_Unit_Price);
+        this.setProduct_Quantity(product_Quantity);
+        this.setSold_Quantity(sold_Quantity);
+        this.setTotal_Sales(total_Sales);
+        this.discount = discount;
+    }
+
+    public void add_Product(String product_ID, String product_Name, float product_Unit_Price, int product_Quantity){
+        this.setProduct_ID(product_ID);
+        this.setProduct_Name(product_Name);
+        this.setProduct_Unit_Price(product_Unit_Price);
+        this.setProduct_Quantity(product_Quantity);
+
+    }
+
+
+
+    public String getProduct_ID() {
+        return product_ID;
+    }
+
+    public void setProduct_ID(String product_ID) {
+        this.product_ID = product_ID;
+    }
+
+    public String getProduct_Name() {
+        return product_Name;
+    }
+
+    public void setProduct_Name(String product_Name) {
+        this.product_Name = product_Name;
+    }
+
+    public float getProduct_Unit_Price() {
+        return product_Unit_Price;
+    }
+
+    public void setProduct_Unit_Price(float product_Unit_Price) {
+        this.product_Unit_Price = product_Unit_Price;
+    }
+
+    public int getProduct_Quantity() {
+        return product_Quantity;
+    }
+
+    public void setProduct_Quantity(int product_Quantity) {
+        this.product_Quantity = product_Quantity;
+    }
+
+    public int getSold_Quantity() {
+        return sold_Quantity;
+    }
+
+    public void setSold_Quantity(int sold_Quantity) {
+        this.sold_Quantity = sold_Quantity;
+    }
+
+    public double getTotal_Sales() {
+        return total_Sales;
+    }
+
+    public void setTotal_Sales(double total_Sales) {
+        this.total_Sales = total_Sales;
+    }
+
+    @Override
+    public String toString(){
+        return "Product Name:   " + getProduct_Name() + "\n" + "Product ID:   " + getProduct_ID() + "\n" + "Unit Price: " + getProduct_Unit_Price() + "\n" + "Product Quantity: " + getProduct_Quantity() + "\n" + "Units Sold: " + getSold_Quantity() + "\n" + "Total Sales:   " + getTotal_Sales() + "\n";
+    }
+}
---
+++ src/Sales.java
@@ -0,0 +1,92 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import javax.swing.*;
+
+
+
+public class Sales {
+
+    public static void main(String[] args) throws Exception{
+        Product myProduct = new Product();
+        ArrayList<Product> products = new ArrayList<Product>();
+
+        StringBuilder str = new StringBuilder();
+
+        final JButton option1 = new JButton("Add Stock");
+        final JButton option2 = new JButton("Purchase");
+        final JButton option3 = new JButton("Product List");
+
+        option1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTextField prodID = new JTextField();
+                JTextField prodName = new JTextField();
+                JTextField unitPrice = new JTextField();
+                JTextField prdQty = new JTextField();
+
+                Object[] fields = {
+                        "Product ID: ", prodID,
+                        "Product Name:  ",prodName,
+                        "Unit Price:    ",unitPrice,
+                        "Product Quantity:  ", prdQty,
+                };
+
+                JOptionPane.showConfirmDialog(null, fields, "Input Product Information", JOptionPane.OK_CANCEL_OPTION);
+
+                String id_Text = prodID.getText();
+                String name_Text = prodName.getText();
+                String price_Text = unitPrice.getText();
+                float price_Val = Float.parseFloat(price_Text);
+                String pQty_Text = prdQty.getText();
+                int pQty_Val = Integer.parseInt(pQty_Text);
+
+                myProduct.add_Product(id_Text,name_Text,price_Val,pQty_Val);
+                products.add(myProduct);
+
+            }
+        });
+        option2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               try {
+                   boolean test = false;
+                   JTextField prodID = new JTextField();
+                   Object[] field1 = {
+                           "Product ID: ", prodID
+                   };
+
+                   JOptionPane.showConfirmDialog(null, field1, "Input Product ID", JOptionPane.OK_CANCEL_OPTION);
+
+                   for(int i = 0; i <= products.size(); i++){
+                       test = prodID.equals(products.get(i).getProduct_ID());
+                   }
+                   if(!test){
+                       throw new DataException(){
+
+                       }
+                   }
+               }
+               catch(Exception d){
+                   JOptionPane.showMessageDialog(null,"Product ID not found.",null, JOptionPane.WARNING_MESSAGE);
+
+               }
+            }
+        });
+        option3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (Product product : products) {
+                    str.append(product.toString());
+                    str.append("\n");
+                }
+                JOptionPane.showMessageDialog(null, str);
+            }
+        });
+        JOptionPane.showOptionDialog(null, "Choose Desired Operation", "Product List",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.PLAIN_MESSAGE,null,new JButton[]{option1,option2,option3},null);
+
+
+
+
+    }
+}
